b9b374e83f1981e37dc86042c7ea0b03e5a88c52
Clyde D'Souza <clyd94@gmail.com>
2025-01-19 15:40:00 +1300

    Jest a11y test setup

--- stories/Button.test.jsx
+++ stories/Button.test.jsx
@@ -1,11 +1,18 @@
 import { render, screen } from "@testing-library/react";
 import React from "react";
 import { Button } from "./Button.jsx";
+import { axe } from "jest-axe";
 
 describe("<Button>", () => {
   it("should display button label", () => {
     render(<Button label={"Greetings"} />);
     const button = screen.getByRole("button");
     expect(button).toHaveTextContent("Greetings");
+  });
+
+  it("should have no a11y violations", async () => {
+    const { container } = render(<Button label={"Greetings"} />);
+    const results = await axe(container);
+    expect(results).toHaveNoViolations();
   });
 });
